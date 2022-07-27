4dd3831d5dccfbac27ecde55aaa71371c06d45d8
David Balcher <David.Balcher@gmail.com>
2022-07-27 14:00:59 -0400

    Initial Commit

---
+++ FeedApp/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  FeedApp
+//
+//  Created by Daveed Balcher on 7/27/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
---
+++ FeedApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FeedApp
+//
+//  Created by Daveed Balcher on 7/27/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
