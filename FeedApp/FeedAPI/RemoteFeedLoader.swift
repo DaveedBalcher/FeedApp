45edf6de65fe9c2d03c042ba45d66dd703225b64
David Balcher <David.Balcher@gmail.com>
2022-08-02 11:22:47 -0400

    More remotefeedloader to production

---
+++ FeedApp/FeedAPI/RemoteFeedLoader.swift
@@ -0,0 +1,28 @@
+//
+//  RemoteFeedLoader.swift
+//  FeedApp
+//
+//  Created by Daveed Balcher on 8/2/22.
+//
+
+import Foundation
+
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
