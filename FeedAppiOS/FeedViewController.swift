38e5cbc10609692c8e644c5eaa0db3d8f79c5183
David Balcher <David.Balcher@gmail.com>
2023-04-27 11:48:49 -0400

    Add startTask for willDisplay TableView Method to account for iOS 15 update

--- FeedAppiOS/FeedViewController.swift
+++ FeedAppiOS/FeedViewController.swift
@@ -65,21 +65,22 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
         cell.feedImageContainer.startShimmering()
 
         let loadImage = { [weak self, weak cell] in
-            guard let self = self else { return }
+            guard let self = self, let cell = cell else { return }
 
-            self.tasks[indexPath] = self.imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
-                let data = try? result.get()
-                let image = data.map(UIImage.init) ?? nil
-                cell?.feedImageView.image = image
-                cell?.feedImageRetryButton.isHidden = (image != nil)
-                cell?.feedImageContainer.stopShimmering()
-            }
+            self.startTask(forRowAt: indexPath, url: cellModel.url, cell: cell)
         }
 
         cell.onRetry = loadImage
         loadImage()
         
         return cell
+    }
+    
+    public override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let cell = cell as? FeedImageCell else { return }
+        
+        let cellModel = tableModel[indexPath.row]
+        startTask(forRowAt: indexPath, url: cellModel.url, cell: cell)
     }
     
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
@@ -95,6 +96,16 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     
     public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
         indexPaths.forEach(cancelTask)
+    }
+    
+    private func startTask(forRowAt indexPath: IndexPath, url: URL, cell: FeedImageCell) {
+        self.tasks[indexPath] = self.imageLoader?.loadImageData(from: url) { [weak cell] result in
+            let data = try? result.get()
+            let image = data.map(UIImage.init) ?? nil
+            cell?.feedImageView.image = image
+            cell?.feedImageRetryButton.isHidden = (image != nil)
+            cell?.feedImageContainer.stopShimmering()
+        }
     }
 
     private func cancelTask(forRowAt indexPath: IndexPath) {
