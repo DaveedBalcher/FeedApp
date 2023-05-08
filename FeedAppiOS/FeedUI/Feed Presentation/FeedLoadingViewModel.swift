a23bc00fc3fc17f86d838fa0c9cf004650e31228
David Balcher <David.Balcher@gmail.com>
2023-05-08 11:31:39 -0400

    Move ViewModels to separate files

---
+++ FeedAppiOS/FeedUI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  FeedAppiOS
+//
+//  Created by Daveed Balcher on 5/8/23.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ FeedAppiOS/FeedUI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  FeedAppiOS
+//
+//  Created by Daveed Balcher on 5/8/23.
+//
+
+import FeedComponents
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
