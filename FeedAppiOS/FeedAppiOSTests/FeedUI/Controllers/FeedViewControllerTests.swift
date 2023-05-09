f326577ee6841c276841065e54b26be96c53736a
David Balcher <David.Balcher@gmail.com>
2023-05-09 11:54:15 -0400

    Reuse table view cells from storyboard configuration

--- FeedAppiOS/FeedAppiOSTests/FeedUI/Controllers/FeedViewControllerTests.swift
+++ FeedAppiOS/FeedAppiOSTests/FeedUI/Controllers/FeedViewControllerTests.swift
@@ -216,6 +216,17 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.cancelledImageURLs, [image0.url, image1.url], "Expected second cancelled image URL request once second image is not near visible anymore")
     }
     
+    func test_feedImageView_doesNotRenderLoadedImageWhenNotVisibleAnymore() {
+    let (sut, loader) = makeSUT()
+    sut.loadViewIfNeeded()
+    loader.completeFeedLoading(with: [makeImage()])
+
+    let view = sut.simulateFeedImageViewNotVisible(at: 0)
+    loader.completeImageLoading(with: anyImageData())
+
+    XCTAssertNil(view?.renderedImage, "Expected no rendered image when an image load finishes after the view is not visible anymore")
+}
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
@@ -249,6 +260,10 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(cell.locationText, image.location, "Expected location text to be \(String(describing: image.location)) for image  view at index (\(index))", file: file, line: line)
         
         XCTAssertEqual(cell.descriptionText, image.description, "Expected description text to be \(String(describing: image.description)) for image view at index (\(index)", file: file, line: line)
+    }
+    
+    private func anyImageData() -> Data {
+        return UIImage.make(withColor: .red).pngData()!
     }
     
     private func makeImage(description: String? = nil, location: String? = nil, url: URL = URL(string: "http://any-url.com")!) -> FeedImage {
