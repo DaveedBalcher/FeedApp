bedee659e45b8f24988e4bd706e47aea0d230ea8
David Balcher <David.Balcher@gmail.com>
2023-06-12 08:54:36 -0400

    Move ErrorView to separate file

---
+++ FeedAppiOS/FeedUI/Controllers/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  FeedAppiOS
+//
+//  Created by Daveed Balcher on 6/12/23.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
