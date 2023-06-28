0b8f50aac12f92669e3886a218e24f75091b80f7
David Balcher <David.Balcher@gmail.com>
2023-06-28 11:32:25 -0400

    Move CoreDataFeedStore implementation of the FeedImageDataStore protocol to production

---
+++ FeedComponents/Feed Cache/Infrastucture/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataFeedStore+FeedImageDataLoader.swift
+//  FeedComponents
+//
+//  Created by Daveed Balcher on 6/28/23.
+//
+
+import Foundation
+
+extension CoreDataFeedStore: FeedImageDataStore {
+
+    public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+
+    }
+
+    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(.none))
+    }
+
+}
