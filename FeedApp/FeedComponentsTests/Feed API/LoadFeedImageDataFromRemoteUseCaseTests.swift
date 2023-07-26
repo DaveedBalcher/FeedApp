6df7e9c76acc4bf151e2c62566ad4b79e6065290
David Balcher <David.Balcher@gmail.com>
2023-07-26 09:17:12 -0400

    Test ImageCommentsMapper in isolation

--- FeedApp/FeedComponentsTests/Feed API/LoadFeedImageDataFromRemoteUseCaseTests.swift
+++ FeedApp/FeedComponentsTests/Feed API/LoadFeedImageDataFromRemoteUseCaseTests.swift
@@ -8,148 +8,79 @@
 import XCTest
 import FeedComponents
 
-class LoadFeedImageDataFromRemoteUseCaseTests: XCTestCase {
+class ImageCommentsMapperTests: XCTestCase {
 
-    func test_init_doesNotPerformAnyURLRequest() {
-        let (_, client) = makeSUT()
-        
-        XCTAssertTrue(client.requestedURLs.isEmpty)
-    }
-    
-    func test_loadImageDataFromURL_requestsDataFromURL() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT(url: url)
-        
-        sut.loadImageData(from: url) { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url])
-    }
-    
-    func test_loadImageDataFromURLTwice_requestsDataFromURLTwice() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT(url: url)
-        
-        sut.loadImageData(from: url) { _ in }
-        sut.loadImageData(from: url) { _ in }
+    func test_map_throwsErrorOnNon2xxHTTPResponse() throws {
+        let json = makeItemsJSON([])
+        let samples = [199, 150, 300, 400, 500]
 
-        XCTAssertEqual(client.requestedURLs, [url, url])
-    }
-    
-    func test_loadImageDataFromURL_deliversConnectivityErrorOnClientError() {
-        let (sut, client) = makeSUT()
-        let clientError = NSError(domain: "a client error", code: 0)
-
-        expect(sut, toCompleteWith: .failure(RemoteFeedImageDataLoader.Error.connectivity), when: {
-            client.complete(with: clientError)
-        })
-    }
-    
-    func test_loadImageDataFromURL_deliversInvalidDataErrorOnNon200HTTPResponse() {
-        let (sut, client) = makeSUT()
-        
-        let samples = [199, 201, 300, 400, 500]
-        
-        samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWith: failure(.invalidData), when: {
-                client.complete(withStatusCode: code, data: anyData(), at: index)
-            })
+        try samples.forEach { code in
+            XCTAssertThrowsError(
+                try ImageCommentsMapper.map(json, from: HTTPURLResponse(statusCode: code))
+            )
         }
     }
-    
-    func test_loadImageDataFromURL_deliversInvalidDataErrorOn200HTTPResponseWithEmptyData() {
-        let (sut, client) = makeSUT()
-        
-        expect(sut, toCompleteWith: failure(.invalidData), when: {
-            let emptyData = Data()
-            client.complete(withStatusCode: 200, data: emptyData)
-        })
-    }
-    
-    func test_loadImageDataFromURL_deliversReceivedNonEmptyDataOn200HTTPResponse() {
-        let (sut, client) = makeSUT()
-        let nonEmptyData = Data("non-empty data".utf8)
-        
-        expect(sut, toCompleteWith: .success(nonEmptyData), when: {
-            client.complete(withStatusCode: 200, data: nonEmptyData)
-        })
-    }
-    
-    func test_cancelLoadImageDataURLTask_cancelsClientURLRequest() {
-        let (sut, client) = makeSUT()
-        let url = URL(string: "https://a-given-url.com")!
 
-        let task = sut.loadImageData(from: url) { _ in }
-        XCTAssertTrue(client.cancelledURLs.isEmpty, "Expected no cancelled URL request until task is cancelled")
-        
-        task.cancel()
-        XCTAssertEqual(client.cancelledURLs, [url], "Expected cancelled URL request after task is cancelled")
-    }
-    
-    func test_loadImageDataFromURL_doesNotDeliverResultAfterCancellingTask() {
-        let (sut, client) = makeSUT()
-        let nonEmptyData = Data("non-empty data".utf8)
+    func test_map_throwsErrorOn2xxHTTPResponseWithInvalidJSON() throws {
+        let invalidJSON = Data("invalid json".utf8)
+        let samples = [200, 201, 250, 280, 299]
 
-        var received = [FeedImageDataLoader.Result]()
-        let task = sut.loadImageData(from: anyURL()) { received.append($0) }
-        task.cancel()
-        
-        client.complete(withStatusCode: 404, data: anyData())
-        client.complete(withStatusCode: 200, data: nonEmptyData)
-        client.complete(with: anyNSError())
-        
-        XCTAssertTrue(received.isEmpty, "Expected no received results after cancelling task")
-    }
-    
-    func test_loadImageDataFromURL_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-        let client = HTTPClientSpy()
-        var sut: RemoteFeedImageDataLoader? = RemoteFeedImageDataLoader(client: client)
-        
-        var capturedResults = [FeedImageDataLoader.Result]()
-        sut?.loadImageData(from: anyURL()) { capturedResults.append($0) }
-        
-        sut = nil
-        client.complete(withStatusCode: 200, data: anyData())
-        
-        XCTAssertTrue(capturedResults.isEmpty)
-    }
-
-    private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedImageDataLoader(client: client)
-        trackForMemoryLeaks(sut, file: file, line: line)
-        trackForMemoryLeaks(client, file: file, line: line)
-        return (sut, client)
-    }
-
-    private func failure(_ error: RemoteFeedImageDataLoader.Error) -> FeedImageDataLoader.Result {
-        return .failure(error)
-    }
-    
-    private func expect(_ sut: RemoteFeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
-        let url = URL(string: "https://a-given-url.com")!
-        let exp = expectation(description: "Wait for load completion")
-
-        sut.loadImageData(from: url) { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedData), .success(expectedData)):
-                XCTAssertEqual(receivedData, expectedData, file: file, line: line)
-                
-            case let (.failure(receivedError as RemoteFeedImageDataLoader.Error), .failure(expectedError as RemoteFeedImageDataLoader.Error)):
-                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
-
-            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
-                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
-                
-            default:
-                XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
-            }
-            
-            exp.fulfill()
+        try samples.forEach { code in
+            XCTAssertThrowsError(
+                try ImageCommentsMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: code))
+            )
         }
-        
-        action()
-        
-        wait(for: [exp], timeout: 1.0)
     }
+
+    func test_map_deliversNoItemsOn2xxHTTPResponseWithEmptyJSONList() throws {
+        let emptyListJSON = makeItemsJSON([])
+        let samples = [200, 201, 250, 280, 299]
+
+        try samples.forEach { code in
+            let result = try ImageCommentsMapper.map(emptyListJSON, from: HTTPURLResponse(statusCode: code))
+
+            XCTAssertEqual(result, [])
+        }
+    }
+
+    func test_map_deliversItemsOn2xxHTTPResponseWithJSONItems() throws {
+        let item1 = makeItem(
+            id: UUID(),
+            message: "a message",
+            createdAt: (Date(timeIntervalSince1970: 1598627222), "2020-08-28T15:07:02+00:00"),
+            username: "a username")
+
+        let item2 = makeItem(
+            id: UUID(),
+            message: "another message",
+            createdAt: (Date(timeIntervalSince1970: 1577881882), "2020-01-01T12:31:22+00:00"),
+            username: "another username")
+
+        let json = makeItemsJSON([item1.json, item2.json])
+        let samples = [200, 201, 250, 280, 299]
+
+        try samples.forEach { code in
+            let result = try ImageCommentsMapper.map(json, from: HTTPURLResponse(statusCode: code))
+
+            XCTAssertEqual(result, [item1.model, item2.model])
+        }
+    }
+
+    // MARK: - Helpers
     
+    private func makeItem(id: UUID, message: String, createdAt: (date: Date, iso8601String: String), username: String) -> (model: ImageComment, json: [String: Any]) {
+        let item = ImageComment(id: id, message: message, createdAt: createdAt.date, username: username)
+
+        let json: [String: Any] = [
+            "id": id.uuidString,
+            "message": message,
+            "created_at": createdAt.iso8601String,
+            "author": [
+                "username": username
+            ]
+        ]
+
+        return (item, json)
+    }
+
 }
