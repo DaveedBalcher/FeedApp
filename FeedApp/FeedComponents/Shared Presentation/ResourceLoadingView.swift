64790c3aebc86cfe7a279e35d5c971776053876b
David Balcher <David.Balcher@gmail.com>
2023-08-15 09:34:04 -0400

    Rename and move ResourceLoadingView and VIewModel to Shared module

---
+++ FeedApp/FeedComponents/Shared Presentation/ResourceLoadingView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceLoadingView.swift
+//  FeedComponents
+//
+//  Created by Daveed Balcher on 8/15/23.
+//
+
+import Foundation
+
+public protocol ResourceLoadingView {
+    func display(_ viewModel: ResourceLoadingViewModel)
+}
