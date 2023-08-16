54acdeb0b91bef959a439a4a5012e5da2fbe2ac4
David Balcher <David.Balcher@gmail.com>
2023-08-16 10:15:15 -0400

    Implement Image Comments UI

---
+++ FeedApp/FeedAppiOS/Image Comment UI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,33 @@
+//
+//  ImageCommentCellController.swift
+//  FeedAppiOS
+//
+//  Created by Daveed Balcher on 8/16/23.
+//
+
+import UIKit
+import FeedComponents
+
+public class ImageCommentCellController: CellController {
+    private let model: ImageCommentViewModel
+
+    public init(model: ImageCommentViewModel) {
+        self.model = model
+    }
+
+    public func view(in tableView: UITableView) -> UITableViewCell {
+        let cell: ImageCommentCell = tableView.dequeueReusableCell()
+        cell.messageLabel.text = model.message
+        cell.usernameLabel.text = model.username
+        cell.dateLabel.text = model.date
+        return cell
+    }
+
+    public func preload() {
+
+    }
+
+    public func cancelLoad() {
+
+    }
+}
---
+++ FeedApp/FeedAppiOS/Image Comment UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  FeedAppiOS
+//
+//  Created by Daveed Balcher on 8/16/23.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
