a53a3c3e075b59efefee4fb7fdf654406d9868d8
David Balcher <David.Balcher@gmail.com>
2023-08-23 10:29:28 -0400

    Set comments title

--- EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+Localization.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+Localization.swift
@@ -11,14 +11,18 @@ import FeedComponents
 
 extension FeedUIIntegrationTests {
     private class DummyView: ResourceView {
-         func display(_ viewModel: Any) {}
-     }
-
-     var loadError: String {
-         LoadResourcePresenter<Any, DummyView>.loadError
-     }
-
-     var feedTitle: String {
-         FeedPresenter.title
-     }
+        func display(_ viewModel: Any) {}
+    }
+    
+    var loadError: String {
+        LoadResourcePresenter<Any, DummyView>.loadError
+    }
+    
+    var feedTitle: String {
+        FeedPresenter.title
+    }
+    
+    var commentsTitle: String {
+        ImageCommentsPresenter.title
+    }
 }
