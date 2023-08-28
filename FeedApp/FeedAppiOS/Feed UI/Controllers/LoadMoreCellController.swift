9b446306dee531a68aebe3465f8b7622a64fa50e
David Balcher <David.Balcher@gmail.com>
2023-08-28 10:17:17 -0400

    Propagate willDisplayCell delegate method calls

---
+++ FeedApp/FeedAppiOS/Feed UI/Controllers/LoadMoreCellController.swift
@@ -0,0 +1,8 @@
+//
+//  LoadMoreCellController.swift
+//  FeedAppiOS
+//
+//  Created by Daveed Balcher on 8/28/23.
+//
+
+import Foundation
--- FeedApp/FeedAppiOSTests/Feed UI/FeedSnapshotTests.swift
+++ FeedApp/FeedAppiOSTests/Feed UI/FeedSnapshotTests.swift
@@ -30,6 +30,15 @@ class FeedSnapshotTests: XCTestCase {
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
     }
     
+//    func test_feedWithLoadMoreIndicator() {
+//        let sut = makeSUT()
+//
+//        sut.display(feedWithFailedImageLoading())
+//
+//        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_FAILED_IMAGE_LOADING_light")
+//        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
+//    }
+    
     // MARK: - Helpers
 
     private func makeSUT() -> ListViewController {
